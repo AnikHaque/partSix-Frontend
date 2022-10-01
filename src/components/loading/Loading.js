c21604ff19b9c962bf9dd7f11557dfaf8ef3b6bc
AnikHaque <ekramul15-811@diu.edu.bd>
2022-10-01 23:00:29 +0600

    added

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import { Circles } from  'react-loader-spinner'
+import './Loading.css';
 export const Loading = () => {
   return (
-    <div>Loading</div>
+    <div>
+    <Circles
+    
+  height="100"
+  width="190"
+  color="#518378"
+  ariaLabel="circles-loading"
+  
+  wrapperClass="hello"
+  visible={true}
+/>
+    </div>
   )
 }
